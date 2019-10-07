0ab6cd355c403d2f9ef820d99410f7ea0b737ed4
WuHaoYuan <joe82714@gmail.com>
2019-10-08 00:17:08 +0800

    first

--- src/bus.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-
-Vue.prototype.$bus = new Vue();  // 新增1個方法bus到Vue的prototype 
-
-// 錯誤提示訊息
-// vm.$bus.$emit('message:push','訊息內容','bootstrap顏色')
-// 'message:push'不動
-// 帶入要顯示的訊息
-// 對應bootsrap的顏色 如 danger、success
