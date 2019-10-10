442dac72fe1ce4b1897ab6f4bb993d5c80132194
WuHaoYuan <joe82714@gmail.com>
2019-10-11 00:13:54 +0800

    fix fillter airbnb

--- src/fillters/unixFormat.js
+++ src/fillters/unixFormat.js
@@ -1,28 +1,28 @@
-export default function (UNIX_timestamp) {
-  let timeStamp = new Date(UNIX_timestamp * 1000); 
-  let dd =timeStamp.getDate();
-  let mm =timeStamp.getMonth()+1; // getMonth()實際是從0開始算所以要加1
-  let yyyy = timeStamp.getFullYear() ;
+export default function (unixTimeStamp) {
+  const timeStamp = new Date(unixTimeStamp * 1000);
+  let dd = timeStamp.getDate();
+  let mm = timeStamp.getMonth() + 1; // getMonth()實際是從0開始算所以要加1
+  const yyyy = timeStamp.getFullYear();
   let hh = timeStamp.getHours();
   let minutes = timeStamp.getMinutes();
   let seconds = timeStamp.getSeconds();
-  if(dd<10){ //讓個位數前加個0
-    dd = '0'+dd ;
+  if (dd < 10) { // 讓個位數前加個0
+    dd = `0+ ${dd}`;
   }
-  if(mm<10){
-    mm = '0'+mm ;
+  if (mm < 10) {
+    mm = `0+ ${mm}`;
   }
-  if(hh<10){
-    hh = '0'+hh ;
+  if (hh < 10) {
+    hh = `0+ ${hh}`;
   }
-  if(minutes<10){
-    minutes = '0'+minutes ;
+  if (minutes < 10) {
+    minutes = `0+ ${minutes}`;
   }
-  if(seconds<10){
-    seconds = '0'+seconds ;
+  if (seconds < 10) {
+    seconds = `0+ ${seconds}`;
   }
   // var time = `${hh}:${minutes}:${seconds}`;
-  let date = `${yyyy}-${mm}-${dd}`;
+  const date = `${yyyy}-${mm}-${dd}`;
   // var fullTime = `${yyyy}-${mm}-${dd} ${time}` ;
-  return date
+  return date;
 }
